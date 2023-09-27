b84fde1f9809f3e698b1d90bcff9c345d3497765
Alusp <alusinejalloh800@gmail.com>
2023-09-27 16:04:49 +0300

    Update Global file

--- lib/global/global.dart
+++ lib/global/global.dart
@@ -1,6 +1,9 @@
 
 
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:users_app/models/user_model.dart';
 
 final FirebaseAuth fAuth = FirebaseAuth.instance;
 User? currentfirebaseUser;
+UserModel? userModelCurrentInfo;
+
